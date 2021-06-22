8ba63b3d55920dff46b06d63cfe2f57f77a616fc
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-06-21 22:02:17 -0700

    #second_anagram? complete and tested

--- anagrams.rb
+++ anagrams.rb
@@ -17,3 +17,20 @@ end
 
 # p first_anagram?("gizmo", "sally")    #=> false
 # p first_anagram?("elvis", "lives")    #=> true
+
+# O(n^2) quadratic time
+# O(n) linear space
+def second_anagram?(string1, string2)
+    string2_array = string2.split("")
+    string1.each do |char|
+        idx2 = string2_array.find_index(char)
+        if !idx2.nil?
+            string2_array.delete_at(idx2)
+        end
+        return true if string2_array.empty?
+    end
+    false
+end
+
+# p first_anagram?("gizmo", "sally")    #=> false
+# p first_anagram?("elvis", "lives")    #=> true
