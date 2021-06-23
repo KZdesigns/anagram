724286e90eff6522f77dea5ffa9d0c35f3ea309a
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-06-22 18:11:18 -0700

    project complete

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Anagram
+Our goal today is to write a method that determines if two given words are anagrams. This means that the letters in one word can be rearranged to form the other word. For example:
+
+```Ruby
+anagram?("gizmo", "sally")    #=> false
+anagram?("elvis", "lives")    #=> true
+Assume that there is no whitespace or punctuation in the given strings.
+```
+
+## Learning Goals
+- Be able to determine the time and space complexity of a method
+- Be able to recognize when and how time or space complexity can be improved
+- Be able to compare different methods and discuss how changing inputs affects each one's overall runtime
--- anagrams.rb
+++ anagrams.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 # O(n!) combinatorial time
 # O(n!) combinatorial space
 def first_anagram?(string1, string2)
@@ -22,7 +24,7 @@ end
 # O(n) linear space
 def second_anagram?(string1, string2)
     string2_array = string2.split("")
-    string1.each do |char|
+    string1.each_char do |char|
         idx2 = string2_array.find_index(char)
         if !idx2.nil?
             string2_array.delete_at(idx2)
@@ -32,5 +34,48 @@ def second_anagram?(string1, string2)
     false
 end
 
-# p first_anagram?("gizmo", "sally")    #=> false
-# p first_anagram?("elvis", "lives")    #=> true
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+
+# O(n*log(n)) linearithmic time
+# O(n) linear space
+def third_anagram?(string1, string2)
+    array1 = string1.split("")
+    array2 = string2.split("")
+
+    array1.sort == array2.sort
+end
+
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
+
+# O(n) linear time
+# O(1) constant space 
+def fourth_anagram?(string1, string2)
+    hash1 = Hash.new(0)
+    hash2 = Hash.new(0)
+
+    string1.each_char { |char| hash1[char] += 1 }
+    string2.each_char { |char| hash2[char] += 1 }
+    hash1 == hash2
+end
+
+# p fourth_anagram?("gizmo", "sally")    #=> false
+# p fourth_anagram?("elvis", "lives")    #=> true
+
+
+
+# O(n) linear time
+# O(1) constant space (same logic as above)
+def fith_anagram?(str1, str2)
+    hash = Hash.new(0)
+    
+    str1.each_char { |char| hash[char] += 1 }
+    str2.each_char { |char| hash[char] += 1 }
+    
+    hash.values.all? { |v| v > 1 }
+end
+
+p fith_anagram?("gizmo", "sally")    #=> false
+p fith_anagram?("elvis", "lives")    #=> true
+
